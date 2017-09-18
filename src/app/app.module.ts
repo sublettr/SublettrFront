b55c7ca4b157c5a8b9c64dc874738aab6b220686
vieck <vieck@purdue.edu>
2017-09-18 18:31:35 -0400

    Started login page with a basic router import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AllMaterialModulesModule } from './all-material.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AllMaterialModulesModule
+    AllMaterialModulesModule,
+    RouterModule.forChild(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
