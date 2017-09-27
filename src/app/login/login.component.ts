96a85b0139a7f2797e5a39f663cdeac9f353bf21
Josh Newlin <jnewlin@purdue.edu>
2017-09-27 17:13:54 -0400

    Merge landing to dev branch (#3)
    * Added landing component and the div frame.
    
    * Background images implemented for options. Faded background and added custom font
    
    * Added Purdue Gradient to search background.
    
    * Created rudementary search bar.
    
    * Added material.io libs.
    
    * Implemented custom material colors.
    
    * Created basic grid of cards.
    
    * Added buttons to toolbar and fine-tuned cards.
    
    * Started login page with a basic router import.
    
    * Routing works.
    
    * Login backgrounds chosen.
    
    * Login page css underway, created input form.
    
    * Login Dialog compiles
    
    * Login dialog appears.
    
    * Added a register button

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../landing/login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
