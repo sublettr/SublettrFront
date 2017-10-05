0d290c0ffb15ac3389432a7cac57d78aba2a9ff0
vieck <vieck@purdue.edu>
2017-10-05 16:08:23 -0400

    Showing template for view sublease.

--- src/app/view-sublease/view-sublease.component.ts
+++ src/app/view-sublease/view-sublease.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewSubleaseComponent implements OnInit {
 
+  sublease = {
+  title: 'Klondike House', url: 'assets/Klondike House.jpg', price: "500", location: "Riatta Place",
+  amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+    title: 'Fitness Center',
+    url: "fit-center"
+  }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+    title: "Free Laundry",
+    url: "laundry"
+  }],
+  desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+};
+
   constructor() { }
 
   ngOnInit() {
