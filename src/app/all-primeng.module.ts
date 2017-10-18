2d96460c9eb4a643e3060722da510e988fa74faa
Michael Vieck <vieck@purdue.edu>
2017-10-18 12:38:04 -0400

    Bootstrap Migration From Material (#15)
    * Added bootstrap css to project.
    
    * Used bootstrap to make every page fullscreen.
    
    * Fixed build error with material md vs mat imports
    
    * Migrated md to mat in every file.
    
    * Added primeng to node package.
    
    * Added primeng module.
    
    * Replaced material top nav with bootstrap top nav
    
    * Added new color palette
    
    * Forgot the actual colors in the last commit
    
    * Cards displaying in a column 2x2.
    
    * Adjusted card margins and rows.
    
    * Moved buttons to the bottom, changed hover colors, made entire card a link.
    
    * Changed brand text to Sublettr.
    
    * Added font awesome icon pack and changed material buttons.
    
    * Updated view single sublease to bootstrap.
    
    * Added sidenav base.
    
    * Added new bg image and changed text color in sidenav
    
    * Added dummy content for now.
    
    * Removed material sidenav, card, and toolbar imports.
    
    * Changed colors of anchor tags and links.
    
    * Disabled horizontal scroll b/c it looks like garbage.
    
    * Landing page takes up full height.
    
    * sidebar takes up full height.

---
+++ src/app/all-primeng.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {DialogModule} from "primeng/primeng";
+
+@NgModule({
+  imports: [
+    DialogModule
+  ],
+  exports: [
+    DialogModule
+  ],
+  declarations: []
+})
+export class AllPrimeNGModulesModule { }
