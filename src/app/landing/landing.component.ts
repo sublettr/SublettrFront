caf392f356dba7e479ed95c217d1f9cd173ce2ae
vieck <vieck@purdue.edu>
2017-10-18 22:43:28 -0400

    Reverted back to bootstrap for the sidenav, it's easier

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,9 +7,24 @@ import { User } from '../_models/user';
 import { UserService } from '../_services/user.service';
 import {SubleaseService} from "../_services/sublet.service";
 import {Router} from "@angular/router";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-landing',
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(-100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateX(-100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
