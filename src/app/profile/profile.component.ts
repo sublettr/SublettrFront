bd10c58cabda1ff10e71ecaaf4006c27519a92c9
vieck <vieck@purdue.edu>
2017-10-19 16:18:27 -0400

    Saved Sublease templete started, horizontal scroll works.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +7,188 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  subleases = [{
+    title: 'Klondike House', url: 'assets/Klondike House.jpg', price: "500", location: "Riatta Place",
+    amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+      title: 'Fitness Center',
+      url: "fit-center"
+    }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+      title: "Free Laundry",
+      url: "laundry"
+    }],
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+    {
+      title: 'Klondike House', price: "500", url: 'assets/Klondike House.jpg', location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House', price: "500", url: "assets/Klondike House.jpg", location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House', price: "500", url: "assets/Klondike House.jpg", location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House', price: "500", url: "assets/Klondike House.jpg", location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House', price: "500", url: "assets/Klondike House.jpg", location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House',
+      price: "500",
+      url: "assets/Klondike House.jpg",
+      location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}]
+      ,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House',
+      price: "500",
+      url: "assets/Klondike House.jpg",
+      location: "Riatta Place",
+      amentities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House',
+      price: "500",
+      url: "assets/Klondike House.jpg",
+      location: "Riatta Place",
+      amentities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House',
+      price: "500",
+      url: "assets/Klondike House.jpg",
+      location: "Riatta Place",
+      amentities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Klondike House',
+      price: "500",
+      url: "assets/Klondike House.jpg",
+      location: "Riatta Place",
+      amentities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    }
+  ];
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
