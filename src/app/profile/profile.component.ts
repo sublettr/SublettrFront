a046cd3d57fe1c882242030abbed838ee8255d25
vieck <vieck@purdue.edu>
2017-10-19 17:38:06 -0400

    Amentities wrap poperly and icons are on the bottom right of the card.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  subleases = [{
+  savedSubleases = [{
     title: 'Klondike House', url: 'assets/Klondike House.jpg', price: "500", location: "Riatta Place",
     amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
       title: 'Fitness Center',
@@ -172,6 +172,64 @@ export class ProfileComponent implements OnInit {
       url: "assets/Klondike House.jpg",
       location: "Riatta Place",
       amentities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    }
+  ];
+
+  postedSubleases = [{
+    title: 'Klondike House', url: 'assets/Klondike House.jpg', price: "500", location: "Riatta Place",
+    amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+      title: 'Fitness Center',
+      url: "fit-center"
+    }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+      title: "Free Laundry",
+      url: "laundry"
+    }],
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+    {
+      title: 'Klondike House', price: "500", url: 'assets/Klondike House.jpg', location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Grant Street Station', price: "500", url: "assets/South Street Station.jpg", location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
+        title: 'Fitness Center',
+        url: "fit-center"
+      }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
+        title: "Free Laundry",
+        url: "laundry"
+      }, {title: "Parking Lot", url: "lot"},
+        {title: "Indoor Pool", url: "in-pool"}, {title: "Outdoor Pool", url: "out-pool"}, {
+          title: "Basketball Court",
+          url: "basketball"
+        }, {title: "Tennis Court", url: "tennis"}],
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      title: 'Grant Street Station', price: "500", url: "assets/South Street Station.jpg", location: "Riatta Place",
+      amenities: [{title: 'Electric', url: "electric"}, {title: 'Water', url: "water"}, {
         title: 'Fitness Center',
         url: "fit-center"
       }, {title: 'Free Parking', url: "parking"}, {title: 'Garage', url: "garage"}, {
