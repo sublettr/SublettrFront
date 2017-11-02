6ea89848f95825b9fa9844fe1c2eef173c96cb05
vieck <vieck@purdue.edu>
2017-11-01 20:47:34 -0400

    Bootstrap added for post screen.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatIconRegistry} from "@angular/material";
 import {DomSanitizer} from "@angular/platform-browser";
+import {Sublease} from "../_models/sublease";
 
 @Component({
   selector: 'app-post',
@@ -12,11 +13,16 @@ export class PostComponent implements OnInit {
   amenities = [{title:'Electric', url: "electric"}, {title:'Water', url:"water"}, {title:'Fitness Center', url:"fit-center"}, {title:'Free Parking', url:"parking"}, {title:'Garage', url:"garage"}, {title:"Free Laundry", url:"laundry"}, {title:"Parking Lot", url:"lot"},
     {title:"Indoor Pool", url:"in-pool"}, {title:"Outdoor Pool", url: "out-pool"}, {title:"Basketball Court", url:"basketball"}, {title:"Tennis Court", url:"tennis"}];
 
+  post: any = {};
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  submitForm() {
+
   }
 
 }
